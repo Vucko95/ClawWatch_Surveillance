310e108a85eccfd12939e709bd02bfa6634e31b7
Ronald Suplina <tyger995@gmail.com>
2023-09-23 18:51:01 +0100

    Add Base Components

--- claw_watch_surveillance/app/page.tsx
+++ claw_watch_surveillance/app/page.tsx
@@ -1,8 +1,74 @@
-import Image from 'next/image'
+"use client"
 
-export default function Home() {
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { useTheme } from "next-themes"
+import { Moon, Sun } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Navbar } from "@/components/ui/navbar"
+import { ModeToggle } from "@/components/ui/mode-toggle"
+import {Dragdrop} from "@/components/ui/dragdrop"
+import {Namespaces} from "@/components/ui/namespaces"
+  export default function Home() {
+    const { setTheme } = useTheme();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-500">
+    <main className="">
+      <Navbar/>
+  <Dragdrop />
+  <Namespaces />
+
+     
+    
+      {/* <Select>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select> */}
+
+{/* <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table> */}
 
     </main>
   )
---
+++ claw_watch_surveillance/components/ui/navbar.tsx
@@ -0,0 +1,26 @@
+"use clinet";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image"
+// import LogoSVG from "./LogoSVG";
+import { ModeToggle } from "@/components/ui/mode-toggle"
+// import { Button } from "./ui/button";
+// import {MobileSidebar} from "@/app/components/mobile-sidebar"
+export const Navbar = () => {
+    return (
+        <div className="w-full z-50">
+            <div className="flex justify-center items-center py-2 px-4 border-b-2 h-16">
+                <Link href="/">
+                    <div className="flex items-center">
+                        {/* <LogoSVG /> */}
+                        <p>Claw Watch Surveillence</p>
+                    </div>
+                    
+                </Link>
+                <div className="absolute top-3 right-3 h-full items-center hidden md:block">
+                    <ModeToggle />
+                </div>
+            </div>
+        </div>
+    )
+}
